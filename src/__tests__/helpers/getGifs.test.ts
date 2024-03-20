7b967677968bae0108399c0ffc29790b58793d67
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-19 22:30:48 -0600

    feat: Add whatwg-fetch and add test on getGifs helper

---
+++ src/__tests__/helpers/getGifs.test.ts
@@ -0,0 +1,16 @@
+import { getGifs } from '../../helpers/getGifs';
+
+describe('Tests on getGifs()', () => { 
+
+  test('should return an array of gifs', async () => { 
+    const gifs = await getGifs('Test React');
+
+    expect( gifs.length ).toBeGreaterThan( 0 );
+    expect( gifs[0] ).toEqual({
+      id: expect.any( String ),
+      title: expect.any( String ),
+      url: expect.any( String )
+    });
+
+  });
+});
